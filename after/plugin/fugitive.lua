87188d730b6ea8196e774828e717b96af221ea7d
Timo Yu <timo.yu.extern@itsg.de>
2023-09-26 16:50:53 +0200

    added quit bind

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,17 +2,14 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = ThePrimeagen_Fugitive,
     pattern = "*",
     callback = function()
-        print("help", vim.bo.ft)
         if vim.bo.ft ~= "fugitive" then
             return
         end
 
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = {buffer = bufnr, remap = false}
-        print("great success", vim.bo.ft, bufnr, vim.inspect(opts))
         vim.keymap.set("n", "<leader>p", function()
             vim.cmd [[ Git push ]]
         end, opts)
--- lua/timo/remap.lua
+++ lua/timo/remap.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>pm", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>qq", "<cmd>q<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
