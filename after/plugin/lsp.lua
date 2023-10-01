15ccbfe1e790b6f0ef95e5d6752b278e515e6514
Timo Yu <ty@21globo.com>
2023-10-01 13:32:04 +0200

    format on save and lsp keybinds added

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,16 +1,36 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
+
+-- auto format on-save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    ensure_installed = {},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+    },
+    formatting = lsp_zero.cmp_format(),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
+})
