d698da00ad7b951318d3061de3cf22b1f77d30f3
Timo Yu <ty@21globo.com>
2024-11-30 11:59:29 +0100

    path in lualine, delete to trash and filename fixed

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -20,7 +20,12 @@ require('lualine').setup {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = {
+            { 'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+        },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
