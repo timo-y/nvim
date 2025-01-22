910c528d910da7583aa49937658173757d07d595
Timo Yu <Timo.Yu@pta.de>
2025-01-22 09:44:19 +0100

    toggleterm powershell/bash

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,8 +1,19 @@
+-- Detect the shell dynamically based on the operating system
+local shell = nil
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    -- Use pwsh (PowerShell Core) if available, otherwise fallback to PowerShell
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+else
+    -- Use bash for Linux and other Unix-like systems
+    shell = "/bin/bash" -- Adjust the path if necessary (e.g., /usr/bin/bash)
+end
+
 require('toggleterm').setup({
     size = 15,
     open_mapping = [[<C-x>]],
     autochdir = true,
     direction = 'horizontal',
+    shell = shell -- Set the dynamically selected shell
 })
 
 function _G.set_terminal_keymaps()
@@ -10,5 +21,5 @@ function _G.set_terminal_keymaps()
     vim.keymap.set('t', '<C-e>', [[<C-\><C-n>]], opts)
 end
 
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
+-- Apply terminal keymaps for toggleterm
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
