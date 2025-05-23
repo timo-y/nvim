21c59fc89c6226b822f29cfb41c472bae24d098e
Yu <t01202@uniper.energy>
2025-05-23 15:35:07 +0200

    codeium removed, mini added

--- lua/timo/plugins/codeium.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- "Exafunction/codeium.vim",
-    -- config = function()
-    --     vim.g.codeium_detect_proxy = false -- Disable proxy-detection
-    --     vim.g.codeium_enabled = true       -- Enable Codeium by default
-    --     vim.g.codeium_idle_delay = 500     -- Set the idle delay (in milliseconds)
-    -- end,
-}
