5b2caa9a57bc86b56ad3c8466e4488a07a832082
Yu <t01202@uniper.energy>
2025-06-16 16:29:22 +0200

    statusbar cleaned

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -22,7 +22,26 @@ require('mini.comment').setup()
 --          ╭─────────────────────────────────────────────────────────╮
 --          │                    STATUS- & TABLINE                    │
 --          ╰─────────────────────────────────────────────────────────╯
-require('mini.statusline').setup()
+require('mini.statusline').setup({
+    content = {
+        active = function()
+            local s = require('mini.statusline')
+            local mode, mode_hl = s.section_mode({ trunc_width = 120 })
+            local filename = s.section_filename({ trunc_width = 140 })
+            local fileinfo = s.section_fileinfo({ trunc_width = 120 })
+            local location = s.section_location({ trunc_width = 75 })
+
+            return s.combine_groups({
+                { hl = mode_hl,                  strings = { mode } },
+                { hl = 'MiniStatuslineFilename', strings = { filename } },
+                '%=',
+                { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                { hl = mode_hl,                  strings = { location } },
+            })
+        end,
+    },
+})
+
 require('mini.tabline').setup()
 --          ╭─────────────────────────────────────────────────────────╮
 --          │                       INDENTSCOPE                       │
