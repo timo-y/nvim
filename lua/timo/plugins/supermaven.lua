55a9febfb9b454188b6686de9ace7a3d67dc1b56
Timo Yu <ty@21globo.com>
2025-10-25 14:38:09 +0200

    supermaven accept C-Tab

--- lua/timo/plugins/supermaven.lua
+++ lua/timo/plugins/supermaven.lua
@@ -1,9 +1,14 @@
 return {
-    {
-      "supermaven-inc/supermaven-nvim",
-      config = function()
-        require("supermaven-nvim").setup({})
-      end,
-    },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<C-Tab>',
+          clear_suggestion = '<C-]>',
+          accept_word = '<C-j>',
+        },
+      }
+    end,
+  },
 }
-
