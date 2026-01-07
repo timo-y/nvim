c855261dfd8c76d35e7dde8dba842fa5126c7e1f
Timo Yu <Timo.Yu@pta.de>
2026-01-07 17:31:04 +0100

    color code updated

--- lua/timo/set.lua
+++ lua/timo/set.lua
@@ -48,7 +48,7 @@ vim.opt.timeoutlen = 300
 vim.opt.mouse = 'a'
 
 vim.opt.colorcolumn = '80'
-vim.cmd [[highlight ColorColumn ctermbg=0 guibg=#222222]]
+vim.cmd [[highlight ColorColumn ctermbg=0 guibg=#262836]]
 
 -- whitespace handling
 vim.o.list = true
